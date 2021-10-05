574bef4e7087756af61b5d1f5a264cdf139ed6de
Lion Behrens <behrens@uni-mannheim.de>
2021-10-05 10:40:35 +0200

    Update mldetection_reproduction.R

--- mldetection_reproduction.R
+++ mldetection_reproduction.R
@@ -1,4 +1,26 @@
-# probabilistic detection of election fraud using machine learning algorithms
+#' Probabilistic Detection of Election Fraud Using Machine Learnign Algorithms
+#' Reproduction Material
+#' Lion Behrens
+#' -----------------------------------------------------------------------------
+
+#' install and load packages
+packages <- c("truncnorm", "e1071")
+
+for (i in 1: length(packages)) {
+  if (is.element(packages[i], installed.packages()[,1]) == FALSE) 
+    install.packages(packages[i], dep = TRUE) 
+}
+
+lapply(packages, library, character.only = T)
+
+#' --------------------------------------
+#  --- 1. synthetic data creation -------
+#' --------------------------------------
+
+  #' ------------------------------------
+  #  --- 1.1 clean data -----------------
+  #' ------------------------------------
+
+    
 
 
-# => erstmal mit github synchronisieren!
